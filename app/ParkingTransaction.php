a79babd035b8cc4c14bae42bc065c751ea99eb85
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-08-10 21:07:34 +0700

    tambah field snapshot

--- app/ParkingTransaction.php
+++ app/ParkingTransaction.php
@@ -9,6 +9,7 @@ class ParkingTransaction extends Model
     protected $fillable = [
         'is_member', 'vehicle_type', 'gate_in_id', 'gate_out_id',
         'time_in', 'time_out', 'barcode_number', 'card_number',
-        'note', 'user_id', 'parking_member_id', 'plate_number', 'fare'
+        'note', 'user_id', 'parking_member_id', 'plate_number',
+        'fare', 'snapshot_in', 'snapshot_out'
     ];
 }
