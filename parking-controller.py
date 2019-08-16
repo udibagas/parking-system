fc87e50d6d8e9b9a815053594b4ab074cdf9ea38
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-08-16 14:14:37 +0700

    async image

--- parking-controller.py
+++ parking-controller.py
@@ -14,7 +14,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
-from kivy.uix.image import Image
+from kivy.uix.image import AsyncImage
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
@@ -171,7 +171,7 @@ Builder.load_string("""
                 size_hint: (1, .1)
                 on_press: root.confirm_exit()
 
-            Image:
+            AsyncImage:
                 source: 'http://localhost/images/logo.jpeg'
                 size_hint: (1, .25)
 
