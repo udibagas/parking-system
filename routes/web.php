30c727dee83bb20e7160dcd2538d3e33e706b7d9
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-08-16 14:28:51 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::group(['middleware' => 'auth.jwt'], function () {
     Route::post('parkingTransaction/takeSnapshot/{parkingTransaction}', 'ParkingTransactionController@takeSnapshot');
     Route::post('parkingTransaction/printTicket/{parkingTransaction}', 'ParkingTransactionController@printTicket');
     Route::post('parkingTransaction/openGate', 'ParkingTransactionController@openGate');
+    Route::put('parkingTransaction/setSudahKeluar/{parkingTransaction}', 'ParkingTransactionController@setSudahKeluar');
     Route::get('parkingTransaction/search', 'ParkingTransactionController@search');
     Route::resource('parkingTransaction', 'ParkingTransactionController')->except(['create', 'edit']);
     Route::resource('notification', 'NotificationController')->only(['index', 'update', 'destroy']);
