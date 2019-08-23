01cc57832e50f769d817b28cd818a6583453ee9d
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-08-23 07:33:19 +0700

    update gate

--- app/Http/Controllers/ParkingGateController.php
+++ app/Http/Controllers/ParkingGateController.php
@@ -9,6 +9,7 @@ use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 use Mike42\Escpos\Printer;
 use PhpSerial\PhpSerial;
+use GuzzleHttp\Client;
 
 class ParkingGateController extends Controller
 {
