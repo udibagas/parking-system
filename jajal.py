75b28cb96dd4d945e570fb9c88d4c276d7aba65e
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-09-02 10:48:09 +0700

    update jajal

--- jajal.py
+++ jajal.py
@@ -4,17 +4,22 @@ import time
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(3)
+
         try:
             s.connect(('192.168.1.100', 5000))
-            while True:
-                try:
-                    s.sendall(b'\xa6STAT\xa9')
-                    print(time.strftime('%T'), s.recv(32))
-                    time.sleep(1)
-                except Exception as e:
-                    print(str(e))
-                    #s.shutdown(socket.SHUT_WR)
-                    break
         except Exception as e:
-            print('reconnecting...')
+            print(time.strftime('%T'), 'reconnecting...')
             time.sleep(3)
+            continue
+        except KeyboardInterrupt:
+            break
+
+        while True:
+            try:
+                s.sendall(b'\xa6STAT\xa9')
+            except Exception as e:
+                print(str(e))
+
+            print(time.strftime('%T'), s.recv(32))
+            time.sleep(1)
+
