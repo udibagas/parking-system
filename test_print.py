7f29519b3393d87dc6d64c731f60481e92a68079
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-09-08 00:35:50 +0700

    jos

---
+++ test_print.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import socket
+import time
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.settimeout(3)
+    try:
+        s.connect(('192.168.1.100', 5000))
+    except Exception as e:
+        print(time.strftime('%T'), 'failed to connect...')
+
+    command = [
+        '\xa6PR4', # start print command, baudrate 19200
+        '\x1b\x61\x49', # align center
+        'TIKET PARKIR\x0a',
+        '\x1b\x21\x10', # double height
+        'MITRATEKNIK',
+        '\x0a\x0a', # 2x new line
+        '\x1b\x21\x00', # normal height
+        '\x1b\x61\x48', # align left
+        'GATE'.ljust(10) + ' : GATE-IN-MOTOR',
+        '\x0a', # new line
+        'TANGGAL'.ljust(10) + ' : ' + time.strftime('%d %b %Y'),
+        '\x0a', # new line
+        'JAM'.ljust(10) + ' : ' + time.strftime('%T'),
+        '\x0a\x0a', # 2x new line
+        '\x1b\x61\x49', # align center
+        'Simpan tiket anda',
+        '\x0a', # new line
+        '\x1b\x69', # cut
+        '\xa9' # end command
+    ]
+
+    s.sendall(format(''.join(command)))
