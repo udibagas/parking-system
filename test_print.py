2d12022b50f500c75ccfd3b5fdc7e8f92e0d5cc1
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-09-08 09:42:34 +0700

    test print

--- test_print.py
+++ test_print.py
@@ -11,20 +11,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(time.strftime('%T'), 'failed to connect...')
 
     command = [
-        '\xa6PR4', # start print command, baudrate 19200
-        '\x1b\x61\x49', # align center
+        '\xa6PR3', # start print command, baudrate 19200
+        '\x1b\x61\x01', # align center
         'TIKET PARKIR\n',
         '\x1b\x21\x10MITRATEKNIK\n\n', # double height
         '\x1b\x21\x00', # normal height
-        '\x1b\x61\x48', # align left
+        '\x1b\x61\x00', # align left
         'GATE    : GATE-IN-MOTOR\n',
         'TANGGAL : ' + time.strftime('%d %b %Y') + '\n',
         'JAM     : ' + time.strftime('%T') + '\n\n',
-        '\x1b\x61\x49', # align center
-        '\x1d\x6bd', # set barcode height = 100
+        '\x1b\x61\x01', # align center
+        # '\x1d\x6bd', # set barcode height = 100
         '\x1d\x77\xa4', # set barcode width = 4, GS w 4
         '\x1d\x48\x50', # set barcode text = below
-        '\x1d\x6b\x04ABC123\x00', #print barcode : GS k 4 [TEXT] NUL
+        '\x1d\x6b\x04', # GS k 4
+        'ABC123'.encode(), # barcode
+        '\x00', # NUL
         '\nSimpan tiket anda\n',
         '\x1d\x56A\x03', # full cut, add 3 lines: GS V 65 3
         '\xa9' # end command
