6559c4eb08563da04f6744107617cc137e5f4aca
Bagas Udi Sahsangka <udibagas@gmail.com>
2020-02-16 16:32:57 +0700

    delete snapshow dont touch timestamp

--- app/Console/Commands/DeleteSnapshotCommand.php
+++ app/Console/Commands/DeleteSnapshotCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\ParkingTransaction;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class DeleteSnapshotCommand extends Command
 {
@@ -42,93 +43,91 @@ class DeleteSnapshotCommand extends Command
             ':age' => $this->argument('age')
         ])->get();
 
-        foreach ($data as $d)
-        {
-            if ($d->snapshot_in && file_exists('./public/'.$d->snapshot_in))
-            {
-                $this->info('Delete file '. $d->snapshot_in);
+        foreach ($data as $d) {
+            if ($d->snapshot_in && file_exists('./public/' . $d->snapshot_in)) {
+                $this->info('Delete file ' . $d->snapshot_in);
 
                 try {
-                    unlink('./public/'.$d->snapshot_in);
-                    $this->info('File '. $d->snapshot_in. ' telah dihapus');
-                    ParkingTransaction::where('snapshot_in', $d->snapshot_in)->update(['snapshot_in' => '']);
+                    unlink('./public/' . $d->snapshot_in);
+                    $this->info('File ' . $d->snapshot_in . ' telah dihapus');
+                    DB::table('parking_transactions')
+                        ->where('snapshot_in', $d->snapshot_in)
+                        ->update(['snapshot_in' => '']);
                 } catch (\Exception $e) {
-                    $this->error('Gagal menghapus file ' . $d->snapshot_in . '. '. $e->getMessage());
+                    $this->error('Gagal menghapus file ' . $d->snapshot_in . '. ' . $e->getMessage());
                 }
             }
 
-            if ($d->snapshot_out && file_exists('./public/'.$d->snapshot_out)) {
-                $this->info('Delete file '. $d->snapshot_out);
+            if ($d->snapshot_out && file_exists('./public/' . $d->snapshot_out)) {
+                $this->info('Delete file ' . $d->snapshot_out);
 
                 try {
-                    unlink('./public/'.$d->snapshot_out);
-                    $this->info('File '. $d->snapshot_out. ' telah dihapus');
-                    ParkingTransaction::where('snapshot_out', $d->snapshot_out)->update(['snapshot_out' => '']);
+                    unlink('./public/' . $d->snapshot_out);
+                    $this->info('File ' . $d->snapshot_out . ' telah dihapus');
+                    DB::table('parking_transactions')
+                        ->where('snapshot_out', $d->snapshot_out)
+                        ->update(['snapshot_out' => '']);
                 } catch (\Exception $e) {
-                    $this->error('Gagal menghapus file ' . $d->snapshot_out . '. '. $e->getMessage());
+                    $this->error('Gagal menghapus file ' . $d->snapshot_out . '. ' . $e->getMessage());
                 }
             }
         }
 
         $years = scandir('./public/snapshot');
 
-        foreach ($years as $year)
-        {
-            if ($year == '.' || $year == '..' || !is_dir('./public/snapshot/'.$year)) {
+        foreach ($years as $year) {
+            if ($year == '.' || $year == '..' || !is_dir('./public/snapshot/' . $year)) {
                 continue;
             }
 
-            $months = scandir('./public/snapshot/'.$year);
+            $months = scandir('./public/snapshot/' . $year);
 
-            foreach($months as $month)
-            {
-                if ($month == '.' || $month == '..' || !is_dir('./public/snapshot/'.$year.'/'.$month)) {
+            foreach ($months as $month) {
+                if ($month == '.' || $month == '..' || !is_dir('./public/snapshot/' . $year . '/' . $month)) {
                     continue;
                 }
 
-                $days = scandir('./public/snapshot/'.$year.'/'.$month);
+                $days = scandir('./public/snapshot/' . $year . '/' . $month);
 
-                foreach ($days as $day)
-                {
-                    if ($day == '.' || $day == '..' || !is_dir('./public/snapshot/'.$year.'/'.$month.'/'.$day)) {
+                foreach ($days as $day) {
+                    if ($day == '.' || $day == '..' || !is_dir('./public/snapshot/' . $year . '/' . $month . '/' . $day)) {
                         continue;
                     }
 
-                    $hours = scandir('./public/snapshot/'.$year.'/'.$month.'/'.$day);
+                    $hours = scandir('./public/snapshot/' . $year . '/' . $month . '/' . $day);
 
-                    foreach ($hours as $hour)
-                    {
-                        if ($hour == '.' || $hour == '..' || !is_dir('./public/snapshot/'.$year.'/'.$month.'/'.$day.'/'.$hour)) {
+                    foreach ($hours as $hour) {
+                        if ($hour == '.' || $hour == '..' || !is_dir('./public/snapshot/' . $year . '/' . $month . '/' . $day . '/' . $hour)) {
                             continue;
                         }
 
                         try {
-                            rmdir('./public/snapshot/'.$year.'/'.$month.'/'.$day.'/'.$hour);
-                            $this->info('Delete directory public/snapshot/'.$year.'/'.$month.'/'.$day.'/'.$hour);
+                            rmdir('./public/snapshot/' . $year . '/' . $month . '/' . $day . '/' . $hour);
+                            $this->info('Delete directory public/snapshot/' . $year . '/' . $month . '/' . $day . '/' . $hour);
                         } catch (\Exception $e) {
                             continue;
                         }
                     }
 
                     try {
-                        rmdir('./public/snapshot/'.$year.'/'.$month.'/'.$day);
-                        $this->info('Delete directory public/snapshot/'.$year.'/'.$month.'/'.$day);
-                        } catch (\Exception $e) {
+                        rmdir('./public/snapshot/' . $year . '/' . $month . '/' . $day);
+                        $this->info('Delete directory public/snapshot/' . $year . '/' . $month . '/' . $day);
+                    } catch (\Exception $e) {
                         continue;
                     }
                 }
 
                 try {
-                    rmdir('./public/snapshot/'.$year.'/'.$month);
-                    $this->info('Delete directory public/snapshot/'.$year.'/'.$month);
+                    rmdir('./public/snapshot/' . $year . '/' . $month);
+                    $this->info('Delete directory public/snapshot/' . $year . '/' . $month);
                 } catch (\Exception $e) {
                     continue;
                 }
             }
 
             try {
-                rmdir('./public/snapshot/'.$year);
-                $this->info('Delete directory public/snapshot/'.$year);
+                rmdir('./public/snapshot/' . $year);
+                $this->info('Delete directory public/snapshot/' . $year);
             } catch (\Exception $e) {
                 continue;
             }
