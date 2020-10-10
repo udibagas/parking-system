dddf22ea927a47324ecbdf20d729f1c337043d4a
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2020-10-11 05:49:49 +0700

    new look

--- resources/js/pages/ControllerLog.vue
+++ resources/js/pages/ControllerLog.vue
@@ -1,37 +1,37 @@
 <template>
-    <div class="log-container" v-html="log"></div>
+	<div
+		class="log-container bg-black text-green-700 p-3 font-mono text-sm overflow-auto"
+		v-html="log"
+	></div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            log: '',
-            requestInterval: null,
-        }
-    },
-    methods: {
-        getLog() {
-            axios.get('controller-log').then(r => this.log = r.data)
-                .catch(e => console.log(e))
-        }
-    },
-    mounted() {
-        this.requestInterval = setInterval(this.getLog, 2000)
-    },
-    destroyed() {
-        clearInterval(this.requestInterval)
-    }
-}
+	data() {
+		return {
+			log: "",
+			requestInterval: null,
+		};
+	},
+	methods: {
+		getLog() {
+			axios
+				.get("controller-log")
+				.then((r) => (this.log = r.data))
+				.catch((e) => console.log(e));
+		},
+	},
+	mounted() {
+		this.requestInterval = setInterval(this.getLog, 2000);
+	},
+	destroyed() {
+		clearInterval(this.requestInterval);
+	},
+};
 </script>
 
 <style lang="scss" scoped>
 .log-container {
-    font-family: monospace;
-    background-color: #000;
-    color: #fff;
-    height: calc(100vh - 240px);
-    overflow: auto;
-    padding: 15px;
+	height: calc(100vh - 155px);
 }
 </style>
