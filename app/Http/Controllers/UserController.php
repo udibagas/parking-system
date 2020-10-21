092452830c5ea22b0616042f6a635792ac2fc166
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2020-10-21 10:48:48 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('role:1');
+        $this->middleware('role:1')->except(['index', 'show']);
     }
 
     /**
