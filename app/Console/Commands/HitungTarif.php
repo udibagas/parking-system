99b3fa6cd842785cb529bd82c744558f321a2f3a
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2020-12-15 21:33:13 +0700

    buat test hitung tarif

---
+++ app/Console/Commands/HitungTarif.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\JenisKendaraan;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class HitungTarif extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tarif:hitung {time_in} {time_out} {jenis_kendaraan}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cek hitung tarif';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $in = new Carbon($this->argument('time_in'));
+        $out = $this->argument('time_out') ? new Carbon($this->argument('time_out')) : Carbon::now();
+        $durasiMenit = $out->diffInMinutes($in);
+        $this->line('Durasi dalam menit = ' . $durasiMenit);
+        $this->line(round($durasiMenit / 60) . ' jam ' . ($durasiMenit % 60) . ' menit');
+
+        $jenisKendaraan = JenisKendaraan::where('nama', $this->argument('jenis_kendaraan'))->first();
+
+        if (!$jenisKendaraan) {
+            $this->error('Tarif tidak ditemukan untuk jenis kendaraan ' . $this->argument('jenis_kendaraan'));
+            return;
+        }
+
+        $tarifMenitPertama = $jenisKendaraan->tarif_menit_pertama;
+        $this->line('Tarif menit pertama = ' . $tarifMenitPertama);
+
+        // tak peduli flat atau progressif berlaku seperti ini
+        if ($durasiMenit <= $jenisKendaraan->menit_pertama) {
+            $ret = $tarifMenitPertama;
+            return;
+        }
+
+        $durasiReal = $durasiMenit - $jenisKendaraan->menit_pertama;
+        $this->line('Durasi real = ' . $durasiReal);
+
+        if ($jenisKendaraan->mode_menginap == JenisKendaraan::MODE_MENGINAP_24JAM) {
+            $hariParkir = ceil($durasiReal / (60 * 24));
+
+            if ($hariParkir == 0) {
+                $hariParkir = 1;
+            }
+        }
+
+        if ($jenisKendaraan->mode_menginap == JenisKendaraan::MODE_MENGINAP_TENGAH_MALAM) {
+            $hariIn = new Carbon($in->format('Y-m-d'));
+            $hariOut = new Carbon($out->format('Y-m-d'));
+            $hariParkir = $hariOut->diffInDays($hariIn);
+
+            if ($hariParkir == 0) {
+                $hariParkir = 1;
+            }
+        }
+
+        $hariMenginap = $hariParkir - 1;
+        $tarifMenginap = $hariMenginap * $jenisKendaraan->tarif_menginap;
+
+        $this->line('Hari parkir = ' . $hariParkir);
+        $this->line('Hari menginap = ' . $hariMenginap);
+        $this->line('Tarif menginap = ' . $tarifMenginap);
+
+        // tarif flat per hari, kena tarif menginap, kena tarif maximal
+        if ($jenisKendaraan->mode_tarif == JenisKendaraan::MODE_TARIF_FLAT) {
+            $ret = $tarifMenitPertama + ($hariParkir * $jenisKendaraan->tarif_flat) + $tarifMenginap;
+        }
+
+        if ($jenisKendaraan->mode_tarif == JenisKendaraan::MODE_TARIF_PROGRESIF) {
+            $tarifMaksimum = $hariMenginap * $jenisKendaraan->tarif_maksimum;
+
+            if ($jenisKendaraan->mode_menginap == JenisKendaraan::MODE_MENGINAP_24JAM) {
+                $tarifHariTerakhir = ceil(($durasiReal % (60 * 24)) / $jenisKendaraan->menit_selanjutnya) * $jenisKendaraan->tarif_menit_selanjutnya;
+                $ret = $tarifMenitPertama + $tarifMaksimum + $tarifHariTerakhir + $tarifMenginap;
+            }
+
+            if ($jenisKendaraan->mode_menginap == JenisKendaraan::MODE_MENGINAP_TENGAH_MALAM) {
+                $menitHariPertama = (new Carbon($in->format('Y-m-d') . ' 24:00:00'))->diffInMinutes($in) - $jenisKendaraan->menit_pertama;
+                $menitHariTerakhir = $out->diffInMinutes((new Carbon($out->format('Y-m-d') . ' 00:00:00')));
+                $tarifHariPertama = ceil($menitHariPertama / $jenisKendaraan->menit_selanjutnya) * $jenisKendaraan->tarif_menit_selanjutnya;
+                $tarifHariTerakhir = ceil($menitHariTerakhir / $jenisKendaraan->menit_selanjutnya) * $jenisKendaraan->tarif_menit_selanjutnya;
+                $ret = $tarifMenitPertama + $tarifMaksimum + $tarifHariPertama + $tarifHariTerakhir + $tarifMenginap;
+            }
+        }
+
+        $this->info('Tarif  = ' . $ret);
+    }
+}
