539325869033ff3e760feaf5a1ec352a26ff6b0a
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-06-21 15:36:18 +0700

    add pos_id

--- app/ParkingTransaction.php
+++ app/ParkingTransaction.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ParkingTransaction extends Model
 {
     protected $fillable = [
-        'is_member', 'vehicle_type',
+        'is_member', 'vehicle_type', 'pos_id',
         'time_in', 'barcode_number', 'card_number',
         'user_id', 'parking_member_id', 'plate_number',
         'fare', 'snapshot_in', 'operator', 'drive_thru'
@@ -16,5 +16,10 @@ class ParkingTransaction extends Model
     public function member()
     {
         return $this->belongsTo(ParkingMember::class, 'parking_member_id');
+    }
+
+    public function pos()
+    {
+        return $this->belongsTo(Pos::class);
     }
 }
