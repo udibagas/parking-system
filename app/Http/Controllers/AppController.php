eb9ae835e064e674608dc6f73c366d487f21018a
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-06-28 22:34:00 +0700

    update

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -31,15 +31,15 @@ class AppController extends Controller
     {
         $nav = [
             ['label' => 'Home', 'icon' => 'el-icon-s-home', 'path' => '/'],
-            ['label' => 'Transaksi', 'icon' => 'el-icon-document-copy', 'path' => 'parking-transaction'],
-            ['label' => 'Member', 'icon' => 'el-icon-bank-card', 'path' => 'member'],
+            ['label' => 'Transaksi', 'icon' => 'el-icon-document-copy', 'path' => '/parking-transaction'],
+            ['label' => 'Member', 'icon' => 'el-icon-bank-card', 'path' => '/member'],
         ];
 
         $adminNav = [
-            ['label' => 'Laporan', 'icon' => 'el-icon-data-analysis', 'path' => 'report'],
-            ['label' => 'User', 'icon' => 'el-icon-user', 'path' => 'user'],
-            ['label' => 'Snapshot', 'icon' => 'el-icon-camera', 'path' => 'snapshot'],
-            ['label' => 'Setting', 'icon' => 'el-icon-setting', 'path' => 'setting'],
+            ['label' => 'Laporan', 'icon' => 'el-icon-data-analysis', 'path' => '/report'],
+            ['label' => 'User', 'icon' => 'el-icon-user', 'path' => '/user'],
+            ['label' => 'Snapshot', 'icon' => 'el-icon-camera', 'path' => '/snapshot'],
+            ['label' => 'Setting', 'icon' => 'el-icon-setting', 'path' => '/setting'],
         ];
 
         return $request->user()->role == 1 ? array_merge($nav, $adminNav) : $nav;
