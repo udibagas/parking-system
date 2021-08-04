e434ca0af23ae7751e55d7cc50933006ca020864
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-08-04 23:02:28 +0700

    change title

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'frontend',
+    title: 'MITRATEKNIK PARKING SYSTEM',
     htmlAttrs: {
       lang: 'en',
     },
