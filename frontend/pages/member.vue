838ac32903b5a752d089da57b1b63cfba69acb58
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-08-08 21:30:22 +0700

    refactor member

--- frontend/pages/member.vue
+++ frontend/pages/member.vue
@@ -1,27 +1,40 @@
 <template>
 	<el-tabs>
-		<el-tab-pane lazy label="MEMBER">
-			<ParkingMember />
+		<el-tab-pane
+			v-for="(tab, i) in tabs.filter((t) => t.visible)"
+			:key="i"
+			lazy
+			:label="tab.label"
+		>
+			<component :is="tab.component" />
 		</el-tab-pane>
-		<el-tab-pane lazy label="GROUP MEMBER" v-if="user.role == 1">
-			<GroupMember />
-		</el-tab-pane>
-		<el-tab-pane lazy label="PEMBAYARAN"><MemberRenewal /></el-tab-pane>
-		<el-tab-pane v-if="user.role == 1" lazy label="LAPORAN PENDAPATAN HARIAN"
-			><MembershipReportDaily
-		/></el-tab-pane>
-		<el-tab-pane v-if="user.role == 1" lazy label="SUMMARY LAPORAN PENDAPATAN"
-			><MembershipReport
-		/></el-tab-pane>
 	</el-tabs>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-
 export default {
-	computed: {
-		...mapState(['user']),
+	data() {
+		return {
+			tabs: [
+				{ component: 'ParkingMember', label: 'MEMBER', visible: true },
+				{
+					component: 'GroupMember',
+					label: 'GROUP MEMBER',
+					visible: this.$auth.user.role == 1,
+				},
+				{ component: 'MemberRenewal', label: 'PEMBAYARAN', visible: true },
+				{
+					component: 'MembershipReportDaily',
+					label: 'LAPORAN PENDAPATAN HARIAN',
+					visible: this.$auth.user.role == 1,
+				},
+				{
+					component: 'MembershipReport',
+					label: 'SUMMARY LAPORAN PENDAPATAN',
+					visible: this.$auth.user.role == 1,
+				},
+			],
+		}
 	},
 }
 </script>
