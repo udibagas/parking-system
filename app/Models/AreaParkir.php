7c3d5292d2bfaad6e9fb92f91962578627f8f9e6
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-18 10:37:55 +0700

    add receivesBroadcastNotificationsOn

--- app/Models/AreaParkir.php
+++ app/Models/AreaParkir.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class AreaParkir extends Model
 {
+    use Notifiable;
+
     protected $fillable = [
         'nama',
         'keterangan',
@@ -17,4 +20,14 @@ class AreaParkir extends Model
     protected $casts = [
         'jenis_kendaraan' => 'json'
     ];
+
+    /**
+     * The channels the user receives notification broadcasts on.
+     *
+     * @return string
+     */
+    public function receivesBroadcastNotificationsOn()
+    {
+        return 'notification';
+    }
 }
