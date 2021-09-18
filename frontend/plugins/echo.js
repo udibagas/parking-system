e08eea5950a552a9ab41352a220998044e77c9a3
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-18 17:23:05 +0700

    add authorizer

--- frontend/plugins/echo.js
+++ frontend/plugins/echo.js
@@ -1,7 +1,7 @@
 import Echo from 'laravel-echo'
 window.Pusher = require('pusher-js')
 
-export default ({ app }, inject) => {
+export default ({ app, $axios }, inject) => {
   inject('echo', (config) => {
     return new Echo({
       broadcaster: 'pusher',
@@ -10,6 +10,19 @@ export default ({ app }, inject) => {
       wsPort: config.port,
       forceTLS: false,
       disableStats: true,
+      authorizer: (channel, options) => {
+        return {
+          authorize: (socketId, callback) => {
+            $axios
+              .$post('/api/broadcasting/auth', {
+                socket_id: socketId,
+                channel_name: channel.name,
+              })
+              .then((response) => callback(false, response))
+              .catch((error) => callback(true, error))
+          },
+        }
+      },
     })
   })
 }
