455bc851f08d64690d0be92a4c14a943129e17fb
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-23 16:55:52 +0700

    pisahin report biar mudah di maintain

---
+++ frontend/components/report/IncomeSummary.vue
@@ -0,0 +1,48 @@
+<template>
+	<el-row :gutter="20" class="mb-3">
+		<el-col :span="8">
+			<el-card>
+				Hari Ini
+				<div class="text-5xl text-purple-700">
+					Rp. {{ $decimal(summary.today) }}
+				</div>
+			</el-card>
+		</el-col>
+		<el-col :span="8">
+			<el-card>
+				Bulan Ini
+				<div class="text-5xl text-blue-700">
+					Rp. {{ $decimal(summary.this_month) }}
+				</div>
+			</el-card>
+		</el-col>
+		<el-col :span="8">
+			<el-card>
+				Total
+				<div class="text-5xl text-orange-700">
+					Rp. {{ $decimal(summary.total) }}
+				</div>
+			</el-card>
+		</el-col>
+	</el-row>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			summary: {},
+		}
+	},
+
+	methods: {
+		getData() {
+			this.$axios.$get('/api/summary').then((r) => (this.summary = r))
+		},
+	},
+
+	created() {
+		this.getData()
+	},
+}
+</script>
