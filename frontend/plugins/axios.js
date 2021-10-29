f7227caa6c7a2ddaa5a81c8af3ef56d069aa8128
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-10-29 11:41:00 +0700

    404 gak usah ditampilin error message

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -3,7 +3,7 @@ import { Message } from 'element-ui'
 export default function ({ $axios, redirect }) {
   $axios.onError((error) => {
     const code = parseInt(error.response && error.response.status)
-    if (code) {
+    if (code && error.response.status != 404) {
       Message({
         message: error.response.data.message,
         type: 'error',
