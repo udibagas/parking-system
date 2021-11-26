05e07d433bef9713f70d74c4b42f6e8bee1d0951
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-11-26 11:45:56 +0700

    benerin namespace

--- app/Console/Commands/ClearTransactionCommand.php
+++ app/Console/Commands/ClearTransactionCommand.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\ParkingTransaction;
+use App\Models\ParkingTransaction;
 use Illuminate\Console\Command;
 
 class ClearTransactionCommand extends Command
