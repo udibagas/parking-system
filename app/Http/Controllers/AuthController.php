1084e31b13f8f01fe17f865637643638bef0cb51
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-05-30 12:33:25 +0700

    buang method logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -46,8 +46,6 @@ class AuthController extends Controller
             'action' => 'LOGOUT'
         ]);
 
-        Auth::logout();
-
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return 'OK';
