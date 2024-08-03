3d96965e83a225660bbbf15c8495c30ee1c02911
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-08-03 17:02:48 +0700

    build

--- nuxt3-fe/components/Kamera.vue
+++ nuxt3-fe/components/Kamera.vue
@@ -1,11 +1,12 @@
 <template>
-  <img
-    style="width: 300px"
-    v-for="kamera in tableData.data"
-    :key="kamera.id"
-    :src="snapshots[kamera.id]"
-    alt="Snapshot"
-  />
+  <div style="display: flex; justify-items: center">
+    <img
+      v-for="kamera in tableData.data?.filter((k) => k.status)"
+      :key="kamera.id"
+      :src="snapshots[kamera.id]"
+      alt="Snapshot"
+    />
+  </div>
 </template>
 
 <script setup>
@@ -13,14 +14,16 @@ const { api, tableData, requestData } = useCrud("/api/kamera");
 const snapshots = ref({});
 
 const getSnapshot = () => {
-  tableData.value.data.forEach(({ id }) => {
-    api(`/api/kamera/test/${id}`).then((r) => {
-      snapshots.value[id] = "data:image/jpeg;base64," + r.snapshot;
+  tableData.value.data
+    ?.filter((k) => k.status)
+    .forEach(({ id }) => {
+      api(`/api/kamera/test/${id}`).then((r) => {
+        snapshots.value[id] = "data:image/jpeg;base64," + r.snapshot;
+      });
     });
-  });
 };
 
-const interval = setInterval(getSnapshot, 1000);
+const interval = setInterval(getSnapshot, 3000);
 
 onBeforeMount(() => {
   requestData();
@@ -28,3 +31,11 @@ onBeforeMount(() => {
 
 onDeactivated(() => clearInterval(interval));
 </script>
+
+<style scoped>
+img {
+  border: 1px solid #eee;
+  width: 300px;
+  height: 300px;
+}
+</style>
---
+++ public/_nuxt/Dk-4cluT.js
@@ -0,0 +1,1 @@
+import"./oPOaDBJV.js";const e=window.setInterval;export{e as s};
