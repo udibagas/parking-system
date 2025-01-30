46b150f5c20a6c11d8a9ef1bc05f42085112a923
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-01-30 17:21:23 +0700

    remove old client

--- frontend/plugins/decimal.js
+++
@@ -1,6 +0,0 @@
-export default ({ app }, inject) => {
-  inject('decimal', (x) => {
-    if (x === undefined || x === null) return 0
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-  })
-}
