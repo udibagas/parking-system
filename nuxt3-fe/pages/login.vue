229c54b40761d33d06c331621e1fcbc10b4b7bfb
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-01-30 18:13:57 +0700

    kasih card and centered

--- nuxt3-fe/pages/login.vue
+++ nuxt3-fe/pages/login.vue
@@ -1,43 +1,50 @@
 <template>
   <el-container>
-    <el-main style="text-align: center; background: #fff">
-      <img
-        src="/images/logo.jpeg"
-        style="width: 70px; margin-top: 80px; border-radius: 5px"
-      />
-      <h2>MITRATEKNIK PARKING</h2>
-      <el-form
-        @submit.native.prevent="doLogin"
-        style="width: 300px; margin: 20px auto 0; text-align: center"
-      >
-        <el-divider>
-          <h3>LOGIN</h3>
-        </el-divider>
+    <el-main
+      style="
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        text-align: center;
+      "
+    >
+      <el-card>
+        <img src="/images/logo.jpeg" style="width: 70px" />
+        <h2>MITRATEKNIK PARKING</h2>
+        <el-form
+          @submit.native.prevent="doLogin"
+          style="width: 300px; margin: 20px auto 0; text-align: center"
+        >
+          <el-divider>
+            <h3>LOGIN</h3>
+          </el-divider>
 
-        <el-form-item>
-          <el-input
-            v-model="email"
-            placeholder="Email/Username"
-            :prefix-icon="User"
-          ></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-input
-            type="password"
-            v-model="password"
-            placeholder="Password"
-            :prefix-icon="Lock"
-          ></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" native-type="submit" style="width: 100%">
-            LOGIN
-          </el-button>
-        </el-form-item>
+          <el-form-item>
+            <el-input
+              v-model="email"
+              placeholder="Email/Username"
+              :prefix-icon="User"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              v-model="password"
+              placeholder="Password"
+              :prefix-icon="Lock"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" native-type="submit" style="width: 100%">
+              LOGIN
+            </el-button>
+          </el-form-item>
 
-        <!-- <a href="http://www.mitrateknik.co.id">www.mitrateknik.co.id</a><br /> -->
-        <el-divider>&copy; {{ year }}</el-divider>
-      </el-form>
+          <!-- <a href="http://www.mitrateknik.co.id">www.mitrateknik.co.id</a><br /> -->
+          <el-divider>&copy; {{ year }}</el-divider>
+        </el-form>
+      </el-card>
     </el-main>
   </el-container>
 </template>
