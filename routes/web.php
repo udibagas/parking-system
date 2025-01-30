de281d991466dfd807adc535f89607c119a7bdc1
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-01-30 18:13:42 +0700

    path tetep pakai yg lama

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('login', function () {
-    return file_get_contents('../nuxt3-fe/.output/public/index.html');
+    return file_get_contents('index.html');
 })->name('login');
 
 Route::get('/{any}', function () {
-    return file_get_contents('../nuxt3-fe/.output/public/index.html');
+    return file_get_contents('index.html');
 })->where('any', '.*');
